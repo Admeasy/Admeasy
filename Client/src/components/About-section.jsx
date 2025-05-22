72518bdc47e74552626b5ad2b20be0d64dbba389
Ahsan <ahsanansari2522011@gmail.com>
2025-05-22 19:07:24 +0530

    /Auto-Sync/

--- Client/src/components/About-section.jsx
+++ Client/src/components/About-section.jsx
@@ -11,9 +11,9 @@ const Section = ({ children }) => {
       variants={fadeUpVariant}
       initial="hidden"
       whileInView="visible"
-      viewport={{ once: false, amount: 0.3 }}
+      viewport={{ once: true, amount: 0.3 }}
       transition={{ duration: 0.7, ease: 'easeOut' }}
-      className="p-5 md:p-10 text-center flex flex-col items-center gap-5 sm:gap-10  border-[0.25px] border-white rounded-4xl shadow-[2px_3px_15px_#00000079]"
+      className="xl:w-75/100 p-5 md:p-10 text-center flex flex-col items-center gap-5 sm:gap-10  border-[0.25px] border-white rounded-4xl shadow-[2px_3px_15px_#00000079]"
     >
       {children}
     </motion.section>
