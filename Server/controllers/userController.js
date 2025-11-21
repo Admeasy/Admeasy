63b98d350f007af2b667361a1624f2b22c76d04a
Ahsan <ahsanansari2522011@gmail.com>
2025-11-21 10:34:17 +0530

    v0.8

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -18,7 +18,11 @@ const forgotPassword = async (req, res) => {
     user.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // 10 min expiry
     await user.save();
 
-    const resetURL = `${process.env.FRONTEND_URL}/reset-password/${token}`;
+    if(process.env.NODE_ENV === 'production'){
+      const resetURL = `https://admeasy.in/reset-password/${token}`;
+    }else{
+      const resetURL = `http://localhost:5173/reset-password/${token}`;
+    }
 
     // Send email
     const transporter = nodemailer.createTransport({
@@ -62,7 +66,7 @@ const forgotPassword = async (req, res) => {
       </p>
 
       <p style="word-break: break-all; font-size: 13px;">
-        <a href="${resetURL}" style="color: #4e6bff;">${resetURL}</a>
+        <a href="${resetURL}" style="color: #4e6bff;">Click here</a>
       </p>
 
       <hr style="margin: 25px 0; border: none; border-top: 1px solid #eee;" />
